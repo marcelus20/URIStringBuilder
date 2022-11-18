bdef979eca8ec4f32a57f88ac95e8c1633e37a97
Marcelus Mantovani <51h2u3fv@anonaddy.me>
2022-11-18 09:00:36 +0000

    Task 2: Started working on the append methods.

--- structured-uri-string-builder/src/main/java/com/marcelus/uristringbuilder/structuredstring/StructuredUriStringBuilder.java
+++ structured-uri-string-builder/src/main/java/com/marcelus/uristringbuilder/structuredstring/StructuredUriStringBuilder.java
@@ -2,31 +2,71 @@ package com.marcelus.uristringbuilder.structuredstring;
 
 import java.util.Optional;
 
-public class StructuredUriStringBuilder {
+public final class StructuredUriStringBuilder {
+
+    private final String scheme;
+    private final String host;
+    private final String port;
+    private final String path;
+    private final String query;
+
+    private StructuredUriStringBuilder(String scheme, String host, String port, String path, String query) {
+        this.scheme = scheme;
+        this.host = host;
+        this.port = port;
+        this.path = path;
+        this.query = query;
+    }
+
+    public StructuredUriStringBuilder() {
+        this.scheme = "";
+        this.host = "";
+        this.port = "";
+        this.path = "";
+        this.query = "";
+    }
 
     public final StructuredUriStringBuilder appendScheme(final Object scheme){
-        return new StructuredUriStringBuilder();
+        return convertObjectToString(scheme)
+                .map(sScheme->new StructuredUriStringBuilder(sScheme, host, port, path, query))
+                .orElse(new StructuredUriStringBuilder(this.scheme,host, port, path, query));
     }
 
     public final StructuredUriStringBuilder appendHost(final Object host){
-        return new StructuredUriStringBuilder();
+        return convertObjectToString(host)
+                .map(sHost->new StructuredUriStringBuilder(scheme, sHost, port, path, query))
+                .orElse(new StructuredUriStringBuilder(scheme, this.host, port, path, query));
     }
 
     public final StructuredUriStringBuilder appendPort(final Object port){
-        return new StructuredUriStringBuilder();
+        return convertObjectToString(port)
+                .map(sPort->new StructuredUriStringBuilder(scheme, host, sPort, path, query))
+                .orElse(new StructuredUriStringBuilder(scheme, host, this.port, path, query));
     }
 
     public final StructuredUriStringBuilder appendPath(final Object path){
-        return new StructuredUriStringBuilder();
+        return convertObjectToString(path)
+                .map(sPath->new StructuredUriStringBuilder(scheme, host, port, sPath, query))
+                .orElse(new StructuredUriStringBuilder(scheme, host, port, this.path, query));
     }
 
     public final StructuredUriStringBuilder appendQuery(final Object key, final Object value){
-        return new StructuredUriStringBuilder();
+        return convertObjectToString(key)
+                .flatMap(sKey->convertObjectToString(value)
+                        .map(sValue->new StructuredUriStringBuilder(scheme, host, port, path, sKey + "=" +sValue)))
+                .orElse(new StructuredUriStringBuilder(scheme, host, port, path, this.query));
+    }
+
+    public StructuredUriStringBuilder appendQuery(final Object query) {
+        return convertObjectToString(query)
+                .map(sQuery->new StructuredUriStringBuilder(scheme, host, port, path, sQuery))
+                .orElse(new StructuredUriStringBuilder(scheme, host, port, path, this.query));
     }
 
     private final Optional<String> convertObjectToString(Object object){
        return Optional.ofNullable(object)
-               .map(String::valueOf);
+               .map(String::valueOf)
+               .map(String::trim);
     }
 
     public final String build(){
--- structured-uri-string-builder/src/test/java/com/marcelus/uristringbuilder/structuredstring/StructuredUriStringBuilderTest.java
+++ structured-uri-string-builder/src/test/java/com/marcelus/uristringbuilder/structuredstring/StructuredUriStringBuilderTest.java
@@ -21,4 +21,111 @@ class StructuredUriStringBuilderTest {
         assertEquals("http://www.foo.bar.bazz/fizz/buzz?ticket=12345", result);
     }
 
+
+    @Test
+    void testingOmittingSchemeScenario(){
+        final String result = new StructuredUriStringBuilder()
+//                .appendScheme("http")
+                .appendHost("www.foo.bar.bazz")
+                .appendPath("fizz")
+                .appendPath("buzz")
+                .appendQuery("ticket", "12345")
+                .build();
+        assertEquals("www.foo.bar.bazz/fizz/buzz?ticket=12345", result);
+    }
+
+
+    @Test
+    void testingOmittingHostScenario(){
+        final String result = new StructuredUriStringBuilder()
+                .appendScheme("http")
+//                .appendHost("www.foo.bar.bazz")
+                .appendPath("fizz")
+                .appendPath("buzz")
+                .appendQuery("ticket", "12345")
+                .build();
+        assertEquals("http://fizz/buzz?ticket=12345", result);
+    }
+
+
+    @Test
+    void testingOmittingPathScenario(){
+        final String result = new StructuredUriStringBuilder()
+                .appendScheme("http")
+                .appendHost("www.foo.bar.bazz")
+//                .appendPath("fizz")
+//                .appendPath("buzz")
+                .appendQuery("ticket", "12345")
+                .build();
+        assertEquals("http://www.foo.bar.bazz?ticket=12345", result);
+    }
+
+
+    @Test
+    void testingOmittingQueryScenario(){
+        final String result = new StructuredUriStringBuilder()
+                .appendScheme("http")
+                .appendHost("www.foo.bar.bazz")
+                .appendPath("fizz")
+                .appendPath("buzz")
+//                .appendQuery("ticket", "12345")
+                .build();
+        assertEquals("http://www.foo.bar.bazz", result);
+    }
+
+    @Test
+    void testingMultipleQueryKeyValuePairsScenario(){
+        final String result = new StructuredUriStringBuilder()
+                .appendScheme("http")
+                .appendHost("www.foo.bar.bazz")
+                .appendQuery("ticket", "12345")
+                .appendQuery("month", "11")
+                .appendQuery("name", "misha")
+                .appendQuery("type", "buyer")
+                .build();
+        assertEquals("http://www.foo.bar.bazz?ticket=12345&month11&name=misha&type=buyer", result);
+    }
+
+    @Test
+    void testingMultipleQueryKeyValuePairsWithEmptyValueScenario(){
+        final String result = new StructuredUriStringBuilder()
+                .appendScheme("http")
+                .appendHost("www.foo.bar.bazz")
+                .appendQuery("ticket")
+                .appendQuery("month")
+                .appendQuery("name", "misha")
+                .appendQuery("type")
+                .build();
+        assertEquals("http://www.foo.bar.bazz?ticket?month?&name=misha&type", result);
+    }
+
+    @Test
+    void testingInvertingTheOrderOfCalls(){
+        final String result = new StructuredUriStringBuilder()
+                .appendScheme("http")
+                .appendQuery("type")
+                .appendQuery("ticket")
+                .appendQuery("month")
+                .appendHost("www.foo.bar.bazz")
+                .appendQuery("name", "misha")
+                .build();
+        assertEquals("http://www.foo.bar.bazz?type&ticket?month?&name=misha", result);
+    }
+
+    @Test
+    void testingReappending(){
+        final String result = new StructuredUriStringBuilder()
+                .appendScheme("http")
+                .appendQuery("type")
+                .appendQuery("id")
+                .appendQuery("ticket")
+                .appendQuery("month")
+                .appendQuery("of")
+                .appendQuery("the")
+                .appendQuery("year")
+                .appendHost("www.foo.bar.bazz")
+                .appendQuery("name", "misha")
+                .build();
+        assertEquals("http://www.foo.bar.bazz?typeid&ticket?monthoftheyear?&name=misha", result);
+    }
 }
